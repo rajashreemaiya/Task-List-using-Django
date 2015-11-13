4e6562476f613e2e5c214639c0c80385c1a72e76
Rajashree Maiya <rk4509@rit.edu>
2015-11-13 09:01:53 -0500

    Modified files with comments added

--- TakskApp/tasks/views.py
+++ TakskApp/tasks/views.py
@@ -6,14 +6,13 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from .models import Item
 from .forms import PostForm,getData
-import datetime
 
 def add_task(request):
+    """Adds a new task to the list."""
     latest_item_list = Item.objects.order_by('title')
     if request.method == 'GET':
         form = PostForm()
     else:
-
         form = PostForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data['Task_Description']
@@ -21,32 +20,33 @@ def add_task(request):
             post = Item.objects.create(title=title,completed=completed)
             return HttpResponseRedirect('/tasks/')
 
-
     return render(request, 'tasks/index.html', {'form': form,'latest_item_list': latest_item_list,})
 
 def update_task(request,item_id):
+    """Updates an existing task in the list."""
     item = get_object_or_404(Item, pk=item_id)
     form = getData(request.POST)
     if request.method == 'POST':
 
         if form.is_valid():
             title = form.cleaned_data['Edit_Task_Description']
-            up = Item.objects.get(id=item_id)
-            up.title = title
-            up.save()
+            updatetask_desc = Item.objects.get(id=item_id)
+            updatetask_desc.title = title
+            updatetask_desc.save()
             return HttpResponseRedirect(".")
     else:
         form = getData()
 
-
     return render(request, 'tasks/detail.html', {'item': item,'form': form,})
 
 def deleteTask(request,item_id):
+    """Delete a task in the list."""
     item = Item.objects.get(pk=item_id)
     item.delete()
     return HttpResponseRedirect('/tasks/')
 
 def markIt(request,item_id):
+    """Mark a task in the list as completed."""
     item = Item.objects.get(pk=item_id)
     item.completed = True
     item.save()
